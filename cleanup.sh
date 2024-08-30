69c831884cff834b3bcc4d49e7c4a169fac3484d
Devfront-end <algorithmelink@gmail.com>
2024-08-30 14:47:48 +0200

    Update cleanup script and improve empty file detection

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf node_modules
+rm -rf client/node_modules
+rm -rf client/build
+rm -rf server/node_modules
+rm -rf server/build
+rm -rf package-lock.json
+rm -rf client/package-lock.json
+rm -rf server/package-lock.json
---
+++ findEmpty.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Finding empty files:"
+find . -type f -empty
+
+echo "Finding empty folders:"
+find . -type d -empty
